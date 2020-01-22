53e90520793d5c3b85b5530180a1cdff8599b930
Tarde <Tarde@N-168>
2020-01-22 12:23:29 -0300

    master

---
+++ Testdeautomatizacion/src/test/java/seachTennis/SeachTest.java
@@ -0,0 +1,25 @@
+package seachTennis;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SeachTest {
+
+	@Test
+	public void searchByTennis() {
+		System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://automationpractice.com/index.php");
+		driver.findElement(By.id("search_query_top")).sendKeys("Tennis");
+		driver.findElement(By.name("submit_search")).click();
+		String textTypeOfResult = driver.findElement(By.className("heading-counter")).getText();
+		System.out.print(textTypeOfResult);
+		Assert.assertEquals(textTypeOfResult, "0 results have been found.");
+		driver.close();
+		driver.quit();
+	
+	}
+}
